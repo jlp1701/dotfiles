0dccef4dbd315ec842be86f1a2e1668e0bd2ef39
jlp1701 <jlp1701@web.de>
2021-04-01 10:55:05 +0200

    ubuntu install script

---
+++ install_ubuntu.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+pushd ~
+
+# install build-essential (apt)
+# install vbox guest-additions
+# reboot the machine
+
+mkdir ~/repos
+
+# install common apt packages
+echo "Installing apt packages ..."
+sudo apt update
+sudo apt install curl firefox gimp git moreutils python3 python3-pip python3-setuptools stow texlive-full
+echo "done."
+
+# install cheat.sh
+curl https://cht.sh/:cht.sh | sudo tee /usr/local/bin/cht.sh
+sudo chmod +x /usr/local/bin/cht.sh
+
+# Git summary
+cd ~/repos
+git clone https://github.com/MirkoLedda/git-summary.git
+cd git-summary
+# sudo cp git-summary /usr/local/bin/git-summary
+ln -s ~/repos/git-summary/git-summary /usr/local/bin/git-summary
+
+# Install vscode and configuration
+
+# Install zsh
+
+# return to previous working dir
+popd
