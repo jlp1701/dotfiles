6e4449a4e8194ccd84a02ce028a187a51d0ee7c6
jlp1701 <jlp1701@web.de>
2021-04-02 16:04:29 +0200

    zsh-autosuggestions

--- install_ubuntu.sh
+++ install_ubuntu.sh
@@ -33,6 +33,7 @@ sudo apt install fonts-powerline zsh
 
 # install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
 # stow the config file
 pushd ~/dotfiles
