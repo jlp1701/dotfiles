0365b45952d15897b45284b64071365240395bd1
jlp1701 <jlp1701@web.de>
2021-04-02 17:46:35 +0200

    ohmyzsh: unattended

--- install_ubuntu.sh
+++ install_ubuntu.sh
@@ -32,7 +32,7 @@ echo "Installing zsh ..."
 sudo apt install fonts-powerline zsh
 
 # install oh-my-zsh
-sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
 # stow the config file
