1bf6620d7f0bfbc758e108610abef0c5403a0cc3
jlp1701 <68975037+jlp1701@users.noreply.github.com>
2021-04-20 22:18:31 +0200

    Added dwm and suckless-tools.

--- install_ubuntu.sh
+++ install_ubuntu.sh
@@ -17,8 +17,8 @@ mkdir -p ~/repos
 
 # install common apt packages
 echo "Installing apt packages ..."
-sudo apt update
-sudo apt install curl firefox gimp git moreutils python3 python3-pip python3-setuptools stow gawk xsel rlwrap texlive-full
+sudo apt update -y
+sudo apt install curl firefox gimp git moreutils python3 python3-pip python3-setuptools stow gawk xsel rlwrap texlive-full -y
 echo "done."
 
 # install cheat.sh
@@ -35,17 +35,19 @@ sudo ln -s ~/repos/git-summary/git-summary /usr/local/bin/git-summary
 # Install vscode and configuration
 
 # install urxvt
-sudo apt install rxvt-unicode xsel
+sudo apt install rxvt-unicode xsel -y
 
 # install terminal extensions
 git clone https://github.com/simmel/urxvt-resize-font
 mkdir -p ~./urxvt/ext
 cp ./urxvt-resize-font/resize-font ~/.urxvt/ext
 
+# install dwm
+sudo apt install dwm suckless-tools -y
 
 # Install zsh
 echo "Installing zsh ..."
-sudo apt install fonts-powerline zsh
+sudo apt install fonts-powerline zsh -y
 
 # install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
